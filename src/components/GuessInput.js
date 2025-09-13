128291106c96b8eca192aefba64bbbc6681a7abe
Isaac gabriel <codebiisaac@gmail.com>
2025-09-12 22:24:45 -0300

    estrutura

---
+++ src/components/GuessInput.js
@@ -0,0 +1,22 @@
+// src/components/GuessInput/GuessInput.js
+import React from 'react';
+import './GuessInput.css';
+
+const GuessInput = ({ guess, onChange, onSubmit, disabled }) => (
+  <div className="guess-input">
+    <input
+      type="number"
+      value={guess}
+      onChange={onChange}
+      min="1"
+      max="100"
+      placeholder="Seu palpite"
+      disabled={disabled}
+    />
+    <button type="button" onClick={onSubmit} disabled={disabled}>
+      Enviar
+    </button>
+  </div>
+);
+
+export default GuessInput;
